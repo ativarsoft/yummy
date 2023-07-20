cd79f892802efb3f3630aa30d917f84376de6a91
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-07-20 18:32:37 -0300

    Only copy DLLs
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- create_release.sh
+++ create_release.sh
@@ -4,6 +4,6 @@
 #zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll avcodec-60.dll avdevice-60.dll avfilter-9.dll avformat-60.dll avutil-58.dll
 
 # ffmpeg depends on many codec libraries out there
-cp -f /mingw32/bin/* .
+cp -f /mingw32/bin/*.dll .
 zip yummy-latest.zip yummy.exe *.dll
 
