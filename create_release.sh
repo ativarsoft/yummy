6d35c88eeff156458f2dbdf6fcb7d40d1e7c7725
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-07-20 17:34:20 -0300

    Include all DLLs in zip file
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- create_release.sh
+++ create_release.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-cp -f /mingw32/bin/{avcodec-60.dll,avdevice-60.dll,avfilter-9.dll,avformat-60.dll,avutil-58.dll} .
+#cp -f /mingw32/bin/{avcodec-60.dll,avdevice-60.dll,avfilter-9.dll,avformat-60.dll,avutil-58.dll} .
+#zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll avcodec-60.dll avdevice-60.dll avfilter-9.dll avformat-60.dll avutil-58.dll
 
 # ffmpeg depends on many codec libraries out there
 cp -f /mingw32/bin/* .
-
-zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll avcodec-60.dll avdevice-60.dll avfilter-9.dll avformat-60.dll avutil-58.dll
+zip yummy-latest.zip yummy.exe *.dll
 
