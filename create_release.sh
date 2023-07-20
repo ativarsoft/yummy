cf32b1e00e5558e5aa0e62c07bff3237d3d73095
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-07-20 16:53:56 -0300

    Added ffmpeg libraries to release file
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- create_release.sh
+++ create_release.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -e
 
-zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll
+cp -f /usr/lib/{avcodec-60.dll,avformat-60.dll,avutil-58.dll} .
+zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll avcodec-60.dll avformat-60.dll avutil-58.dll
 
