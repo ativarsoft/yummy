a53e8e002cc6437126069d59c56b8fe1dc745e08
Mateus de Lima Oliveira <mateus@ativarsoft.com>
2023-07-20 17:10:37 -0300

    Adjusted toolchain prefix
    Signed-off-by: Mateus de Lima Oliveira <mateus@ativarsoft.com>

--- create_release.sh
+++ create_release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-cp -f /usr/bin/{avcodec-60.dll,avdevice-60.dll,avfilter-9.dll,avformat-60.dll,avutil-58.dll} .
+cp -f /mingw32/bin/{avcodec-60.dll,avdevice-60.dll,avfilter-9.dll,avformat-60.dll,avutil-58.dll} .
 zip yummy-latest.zip yummy.exe in_ffmpeg.dll out_null.dll avcodec-60.dll avdevice-60.dll avfilter-9.dll avformat-60.dll avutil-58.dll
 
